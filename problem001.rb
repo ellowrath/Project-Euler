5bcd0e8a2b08d226c30b3513218b685d0b9bd31e
Matt <mh2296@uncw.edu>
2018-01-02 23:35:42 -0500

    Add files via upload

---
+++ problem001.rb
@@ -0,0 +1,15 @@
+# Project Euler solution, Problem 1
+# Matt Hivner
+
+count = 0
+#sum = 0
+until count == 1000
+    if count % 3 == 0
+        sum = sum + count
+    elsif count % 5 == 0
+        sum = sum + count
+    end
+    count = count + 1
+end
+
+puts sum
